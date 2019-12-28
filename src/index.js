5ce9d54572aff2ea7fa4cc092de439fa68f66be1
Andrea Cattaneo <andrea@cattaneo.dev>
2019-12-28 19:19:38 +0100

    Update build process

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+module.exports.antlr = require('antlr4');
 module.exports.ll1 = require('./ll1');
 module.exports.parser = require('./parser');
 module.exports.GrammarlangLexer = require('../grammarlang/grammarlangLexer').grammarlangLexer;
