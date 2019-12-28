e8ff13290b3cae11a29c9d93da29799d65913884
Andrea Cattaneo <andrea@cattaneo.dev>
2019-12-28 14:43:10 +0100

    Fix exports

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 module.exports.ll1 = require('./ll1');
 module.exports.parser = require('./parser');
+module.exports.GrammarlangLexer = require('../grammarlang/grammarlangLexer').grammarlangLexer;
+module.exports.GrammarlangParser = require('../grammarlang/grammarlangParser').grammarlangParser;
