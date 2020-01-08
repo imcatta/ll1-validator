9be494238fdca252952f76143e160eef0101d80a
matteoloca <matteo.locatelli96@gmail.com>
2020-01-08 15:41:28 +0100

    added memo-ll1

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 module.exports.ll1 = require('./ll1');
+module.exports.ll1 = require('./memo-ll1');
 module.exports.parser = require('./parser');
 module.exports.GrammarlangLexer = require('../grammarlang/grammarlangLexer').grammarlangLexer;
 module.exports.GrammarlangParser = require('../grammarlang/grammarlangParser').grammarlangParser;
